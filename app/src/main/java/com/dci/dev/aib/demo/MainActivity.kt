9582cdc45186df8d80778c5a6b1706c8e69aeefa
Cristian Dita <cristian.dita@orange.com>
2020-03-22 17:59:22 +0200

    Fix demo app package Remove unused resurces

--- app/src/main/java/com/dci/dev/aib/demo/MainActivity.kt
+++ app/src/main/java/com/dci/dev/aib/demo/MainActivity.kt
@@ -3,12 +3,9 @@ package com.dci.dev.aib.demo
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 import com.dci.dev.appinfobadge.AppInfoBadge
-import com.dci.dev.appinfobadge.R
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.reflect.Field
 
@@ -68,22 +65,6 @@ class MainActivity : AppCompatActivity() {
 			supportFragmentManager.beginTransaction()
 				.replace(R.id.fragment, randomFragment)
 				.commit()
-		}
-	}
-
-	override fun onCreateOptionsMenu(menu: Menu): Boolean {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		menuInflater.inflate(R.menu.menu_main, menu)
-		return true
-	}
-
-	override fun onOptionsItemSelected(item: MenuItem): Boolean {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		return when (item.itemId) {
-			R.id.action_settings -> true
-			else -> super.onOptionsItemSelected(item)
 		}
 	}
 }
